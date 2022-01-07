9cb1ecda50d6c6602647d3602c7771f58e8c17c2
Houman <houmansnt@gmail.com>
2022-01-07 16:41:29 +0330

    show tasks list

--- src/App.js
+++ src/App.js
@@ -5,40 +5,42 @@ import TodoList from './blockchain/build/contracts/TodoList.json'
 
 const App = () => {
   const [account, setAccount] = useState();
-  // const [contactList, setContactList] = useState();
-  // const [contacts, setContacts] = useState([]);
-
-  const loadContract = () => {
-    console.log(TodoList)
-  }
+  const [todoList, setTodoList] = useState([]);
 
   const load = async () => {
     const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
     const accounts = await web3.eth.requestAccounts();
     setAccount(accounts[0]);
-    // // Instantiate smart contract using ABI and address.
-    // const contactList = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
-    // // set contact list to state variable.
-    // setContactList(contactList);
-    // // Then we get total number of contacts for iteration
-    // const counter = await contactList.methods.count().call();
-    // // iterate through the amount of time of counter
-    // for (var i = 1; i <= counter; i++) {
-    //     // call the contacts method to get that particular contact from smart contract
-    //     const contact = await contactList.methods.contacts(i).call();
-    //     // add recently fetched contact to state variable.
-    //     setContacts((contacts) => [...contacts, contact]);
-    // }
+
+    const contractInstance = new web3.eth.Contract(TodoList.abi, '0x8Aa73FD61F8BDEdBd7bd8484657545936bE1E77d');
+
+
+    const counter = await contractInstance.methods.taskCount().call();
+
+    const tempList = []
+    for (var i = 1; i <= counter; i++) {
+      const todo = await contractInstance.methods.tasks(i).call();
+      tempList.push(todo)
+    }
+    setTodoList(tempList)
   }
 
   useEffect(() => {
     load()
-    loadContract()
   }, [])
 
   return (
     <div>
       your account is {account}
+      <br />
+      <h2>TodoList</h2>
+      <ul>
+        {todoList.map(item => (
+          <li>
+            {item.content}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
