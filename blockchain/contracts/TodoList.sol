7f06cd9e0d2c0364646b8601ca5ff37d5cae777c
Houman <houmansnt@gmail.com>
2022-01-07 14:20:26 +0330

    create task function  on contract

--- blockchain/contracts/TodoList.sol
+++ blockchain/contracts/TodoList.sol
@@ -2,4 +2,22 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("This is task number 1");
+        createTask("This is task number 2");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
