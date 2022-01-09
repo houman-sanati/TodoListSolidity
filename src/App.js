3de9c0e4b6ab7a32927907a591d66c399c945da7
Hooman Sanati <houmansnt@gmail.com>
2022-01-09 15:57:09 +0330

    ui structure chnages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import { useEffect, useState } from 'react'
+import { Card, Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import Web3 from 'web3'
 import TodoList from './blockchain/build/contracts/TodoList.json'
 
@@ -12,7 +13,7 @@ const App = () => {
     const accounts = await web3.eth.requestAccounts();
     setAccount(accounts[0]);
 
-    const contractInstance = new web3.eth.Contract(TodoList.abi, '0x8Aa73FD61F8BDEdBd7bd8484657545936bE1E77d');
+    const contractInstance = new web3.eth.Contract(TodoList.abi, '0x87CcC9473521043558Cd4Cb99c366687Af5857D2');
 
 
     const counter = await contractInstance.methods.taskCount().call();
@@ -30,18 +31,46 @@ const App = () => {
   }, [])
 
   return (
-    <div>
-      your account is {account}
-      <br />
-      <h2>TodoList</h2>
-      <ul>
-        {todoList.map(item => (
-          <li>
-            {item.content}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">TodoList</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link>Todos</Nav.Link>
+            <Nav.Link>Add</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link>{account}</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container>
+        <Card className="my-3">
+          <Card.Body>
+            <Row>
+              <Col md={4}>Id</Col>
+              <Col md={4}>Content</Col>
+              <Col md={4}>Completed</Col>
+            </Row>
+          </Card.Body>
+        </Card>
+        <Row>
+          {todoList.map(item => (
+            <Col md={12} className="mt-1">
+              <Card>
+                <Card.Body>
+                  <Row>
+                    <Col md={4}>{item.id}</Col>
+                    <Col md={4}>{item.content}</Col>
+                    <Col md={4}>{item.completed ? '✔' : '❌'}</Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
   );
 }
 
